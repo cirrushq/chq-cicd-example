5f421d26ddbae3d32d9740f9fba7bad5ac766638
John Walker <49305993+johnwalkercirrushq@users.noreply.github.com>
2021-11-04 02:23:17 +0000

    Reverting Test

--- helloworld.py
+++ helloworld.py
@@ -5,9 +5,5 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello World!</h1>"
 
-@app.route("/hellodevops")
-def hellodevops():
-	return "<h1>Hello Edinburgh DevOps!</h1>"
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
